db9426d8551671bff5fa3b1e602a3b781aadb6b6
Bruno Queiros <brunoofq@gmail.com>
2026-01-07 14:41:26 +0100

    sort countries

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className="App flex">
       <div className='min-h-full w-64 z-50 overflow-x-hidden border-b border-neutral-200 bg-white p-0 backdrop-blur-md dark:border-neutral-800 dark:bg-neutral-900 md:fixed md:left-0 md:h-[calc(100vh-65px)] md:border-r md:p-2 md:pb-0'>
-        {Object.keys(countriesCount).map(country => (
+        {Object.keys(countriesCount).sort().map(country => (
           <div key={`country-count-${country}`} className="flex items-center space-x-1 overflow-y-auto pb-1 pt-1 md:flex-col md:space-x-0 md:space-y-1 md:px-0 md:pt-0">
             <a href="/brazil/" className="flex w-full items-center justify-between space-x-3 rounded-md px-2 py-3 text-sm text-neutral-600 ring-offset-transparent hover:bg-neutral-200 hover:text-neutral-950 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring active:translate-y-0.5 dark:text-neutral-300 dark:hover:bg-neutral-700/40 dark:hover:text-white md:py-2">
               <div className="flex items-center gap-1.5">
