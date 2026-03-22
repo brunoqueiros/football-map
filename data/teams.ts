19adf8479fba984f19dc1548d3ccb3a91070ee73
Bruno Queiros <brunoofq@gmail.com>
2026-03-22 12:44:52 -0300

    add spain La Liga

--- data/teams.ts
+++ data/teams.ts
@@ -1,2 +1,3 @@
 export { default as brazil } from './brazil-teams.json';
 export { default as netherlands } from './netherlands-teams.json';
+export { default as spain } from './spain-teams.json';
--- data/venues.ts
+++ data/venues.ts
@@ -1,2 +1,3 @@
 export { default as brazil } from './brazil-venues.json';
 export { default as netherlands } from './netherlands-venues.json';
+export { default as spain } from './spain-venues.json';
